c199e0dbc1fe7f173f07ffdf90a16a7accd25690
Shinra Yamakawa <13755428+OldBigBuddha@users.noreply.github.com>
2023-04-09 01:44:07 +0900

    fix: file path in graphql codegen script

--- packages/api/scripts/generate-typing.ts
+++ packages/api/scripts/generate-typing.ts
@@ -5,11 +5,11 @@
 import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
 import { join } from 'path';
 
-const SRC_DIR = join(process.cwd(), '..', 'src');
+const SRC_DIR = join(__dirname, '..', 'src');
 
 const definitionsFactory = new GraphQLDefinitionsFactory();
 definitionsFactory.generate({
-  typePaths: [join(SRC_DIR, '**/*.graphql')],
+  typePaths: [join(SRC_DIR, '**', '*.graphql')],
   path: join(SRC_DIR, 'graphql.autogen.ts'),
   outputAs: 'class',
 });
