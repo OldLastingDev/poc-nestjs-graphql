a39cb5605eae879aa6852cf783571af548aaad87
Shinra Yamakawa <13755428+OldBigBuddha@users.noreply.github.com>
2023-04-10 02:31:25 +0900

    feat(task): implement didTask mutation

--- packages/api/src/task/task.service.ts
+++ packages/api/src/task/task.service.ts
@@ -6,10 +6,10 @@ import { TaskUsecase } from './task.usecase';
 
 @Injectable()
 export class TaskService {
-  constructor(private readonly taskUsecase: TaskUsecase) {}
+  constructor(private readonly usecase: TaskUsecase) {}
 
   async create(input: CreateTaskInput): Promise<TaskEntity> {
-    const entity = await this.taskUsecase.create({
+    const entity = await this.usecase.create({
       title: input.title,
       description: input.description,
       deadlineAt: input.deadlineAt === undefined ? undefined : new Date(input.deadlineAt),
@@ -18,21 +18,30 @@ export class TaskService {
   }
 
   async findAll(): Promise<TaskEntity[]> {
-    return await this.taskUsecase.findAll();
+    return await this.usecase.findAll();
   }
 
   async findByUlid(ulid: ULID): Promise<TaskEntity | undefined> {
-    const entity = await this.taskUsecase.findByUlid(ulid);
+    const entity = await this.usecase.findByUlid(ulid);
     return entity;
   }
 
-  async update(ulid: ULID, input: UpdateTaskInput): Promise<TaskEntity> {
-    const entity = await this.taskUsecase.findByUlid(ulid);
+  async didByUlid(ulid: ULID): Promise<TaskEntity> {
+    const entity = await this.usecase.findByUlid(ulid);
     if (entity === undefined) {
       throw new Error(`Not found a task: ${ulid}`);
     }
 
-    return await this.taskUsecase.update(entity, {
+    return await this.usecase.did(entity);
+  }
+
+  async update(ulid: ULID, input: UpdateTaskInput): Promise<TaskEntity> {
+    const entity = await this.usecase.findByUlid(ulid);
+    if (entity === undefined) {
+      throw new Error(`Not found a task: ${ulid}`);
+    }
+
+    return await this.usecase.update(entity, {
       title: input.title,
       description: input.description,
       deadlineAt: input.deadlineAt ? new Date(input.deadlineAt) : undefined,
@@ -40,11 +49,11 @@ export class TaskService {
   }
 
   async removeByUlid(ulid: ULID): Promise<void> {
-    const entity = await this.taskUsecase.findByUlid(ulid);
+    const entity = await this.usecase.findByUlid(ulid);
     if (entity === undefined) {
       throw new Error(`Not found a task: ${ulid}`);
     }
 
-    await this.taskUsecase.trash(entity);
+    await this.usecase.trash(entity);
   }
 }
